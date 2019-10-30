5a061db37ba580ef776953704afa3dbb626b49f6
Jorge Marin <marinjorge1999@gmail.com>
2019-10-30 12:25:07 -0700

    Minor edits to prototype.cpp

--- prototype/prototype.cpp
+++ prototype/prototype.cpp
@@ -25,14 +25,14 @@ int main(int argc, char* argv[]) {
 	if (pid < 0) { //error occurred
 		perror("Error upon creating fork");
 	}
-	else if (pid == 0) { //this code is only executed by child process
+	else if (pid == 0) { //this code is only executed by child process, 0 = child process
 		cout << "This is the child process. pid = " <<  getpid() << endl;
 		cout << "Executing " << updatedArgs[0] << endl;
 		execvp(updatedArgs[0], updatedArgs);
-		cout << "This is code after execvp and should not be executed" << endl;
+		cout << "This is code after execvp and should not be executed" << endl; //upon completion of the child process, the child process is killed (this is why we fork)
 	}
-	else { //this code is only executed by parent process
-		waitpid(pid, NULL, 0);
+	else { //this code is only executed by parent process, which is waiting for the child to finish
+		waitpid(pid, NULL, 0); //parent waiting
 		cout << "This is the parent process. pid = " << getpid() << endl;
 	}
 	cout << "This should be the last line outputted as the program continues its code" << endl;
@@ -51,16 +51,5 @@ void commandParser(int argc, char* argv[]) {
 				//if it is, ++connectorCount and next arg is going to be another executable
 				//if its none of these, then its an argument so ++argument 
 		}
-	}
 }
-
-
-
-
-
-
-
-
-
-
-
+}
