7cbe37f48206295496a704f509845a4542e53b9d
Jorge Marin <marinjorge1999@gmail.com>
2019-10-30 15:29:22 -0700

    LAst commit before push and pull

--- prototype/prototype.cpp
+++ prototype/prototype.cpp
@@ -13,22 +13,29 @@ void commandParser(int argc, char* argv[]);
 int main(int argc, char* argv[]) {
 	string fullcommand;
 
-	cout << "Number of args: " << argc << endl;
+	cout << "Number of args: " << argc << endl; //This tests that the arguments passed in upon execution is as expected
 	cout << "Contents in array: " << endl;
 	for (unsigned int i = 0; i < argc; i++) {
 		cout << argv[i] << " ";
 	}
 	cout << endl;
 	char* updatedArgs[] = {argv[1], NULL};
+
+	/*We learned that if we want to execute a file from within the parent process, there are 3 main steps:
+ 		1) fork the current process
+		2) Child process executes the executable using execvp()
+		3) Parent process waits for the child process to finish executing, then parent continues with any following lines of code */
 	cout << "Beginning a fork" << endl;
-	pid_t pid = fork();
-	if (pid < 0) { //error occurred
-		perror("Error upon creating fork");
+	pid_t pid = fork(); //Step 1 starts here
+	
+	if (pid < 0) { //fork() returns a negative number if an error occurred
+		perror("Error upon creating fork"); //perror is what we use to handle a fork error
 	}
 	else if (pid == 0) { //this code is only executed by child process, 0 = child process
 		cout << "This is the child process. pid = " <<  getpid() << endl;
 		cout << "Executing " << updatedArgs[0] << endl;
-		execvp(updatedArgs[0], updatedArgs);
+		execvp(updatedArgs[0], updatedArgs); //updatedArgs holds all the information passed in by argv except the first item as the 
+						     //first item wsa the executable used to begin main, so not needed for this execution
 		cout << "This is code after execvp and should not be executed" << endl; //upon completion of the child process, the child process is killed (this is why we fork)
 	}
 	else { //this code is only executed by parent process, which is waiting for the child to finish
@@ -45,11 +52,11 @@ void commandParser(int argc, char* argv[]) {
 	int connectorCount = 0;
 	int argumentCount = 0;
 	if (argc > 0) {
-		++argumentCount; //first command will be argument
+		++executableCount; //first command will be executable
 		for (unsigned int i = 0; i < argc; ++i) {//iterate through everything in the argv.
-				//for first and following arguments, check last char in string to see if it is ';' connector
+				//for first and following executables, check last char in string to see if it is ';' connector
 				//if it is, ++connectorCount and next arg is going to be another executable
 				//if its none of these, then its an argument so ++argument 
 		}
-}
-}
+	}
+
