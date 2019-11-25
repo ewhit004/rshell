b5ba193e554129f60d90e03d71b59390820c64d1
Jorge Marin <marinjorge1999@gmail.com>
2019-11-25 09:44:48 -0800

    Fixed linking issues in unitTest

--- unit_tests/unitTest.hpp
+++ unit_tests/unitTest.hpp
@@ -1,10 +1,10 @@
 #ifndef __UNIT_TEST_HPP__
 #define __UNIT_TEST_HPP__
 #include "gtest/gtest.h"
-#include "/class/classes/jmari016/assignment-rshell/src/Execute.hpp"
-#include "/class/classes/jmari016/assignment-rshell/src/ExecuteAND.hpp"
-#include "/class/classes/jmari016/assignment-rshell/src/ExecuteOR.hpp"
-#include "/class/classes/jmari016/assignment-rshell/src/ExecuteSEMI.hpp"
+#include "../src/Execute.hpp"
+#include "../src/ExecuteAND.hpp"
+#include "../src/ExecuteOR.hpp"
+#include "../src/ExecuteSEMI.hpp"
 
 TEST(ExecuteTest, ExecuteExit) {
 	//char* arr[] = {"ls", "-a"};
