560d4bcfa4a3f07e41c078811aaa6fb10fb2b680
Jorge Marin <marinjorge1999@gmail.com>
2019-11-28 02:50:58 -0800

    Nested precedence works

--- src/main.cpp
+++ src/main.cpp
@@ -9,19 +9,19 @@
 #include <string.h>
 #include <cstring>
 #include <stdlib.h>
-#include <iterator> 
 #include <map>
 #include <stdio.h>
 using namespace std;
 
-rShell* parse(string targetCommand, vector<string> &quotedData, vector<string> &parenthesisData);
+rShell* parse(string targetCommand, vector<string> &quotedData, map<int, string> &parenthesisMap);
 void print();
-string extractParenthesis(string userInput, vector<string> &parenthesisData);
+string extractParenthesis(string userInput, int &key,  map<int, string> &parenthesisMap);
 
 int main() {
 	string userInput;
 	bool temp;
-	//map<int, string> parenthesisMap
+	int key = 0;
+	map<int, string> parenthesisMap;
 	restart:
 	print();
 	getline(cin, userInput);
@@ -33,7 +33,7 @@ int main() {
 		if(userInput != "") {
 			string targetString; //if command has a quoted string(s), temporarly stored here and then into vector
 			vector<string> quotedData;
-			vector<string> parenthesisData;
+			//vector<string> parenthesisData;
 			size_t found = userInput.find('"');
 			int firstIndex;
 			int secIndex;
@@ -45,6 +45,14 @@ int main() {
 				quotedData.push_back(targetString);
 				found = userInput.find('"');
 			}
+			found = userInput.find('['); //looks for any bracket test commands in userinput and replaces them with just "test"
+                        while (found != string::npos) {
+                                firstIndex = userInput.find_first_of('[');
+                                userInput.replace(firstIndex, 1, "test");
+                                secIndex = userInput.find_first_of(']');
+                                userInput.erase(secIndex, 1);
+                                found = userInput.find('[');
+                        }
 			found = userInput.find('(');//looks for parenthesis in the userInput
 			if (found != string::npos) {
 				int x = 0;
@@ -59,33 +67,21 @@ int main() {
 					cout << "Invalid Input" << endl;
 					goto restart;
 				}
+				found = userInput.find('(');
 				while (found != string::npos) {
-					firstIndex = userInput.find_first_of('(');
-					secIndex = userInput.find_first_of(')');
-					targetString = userInput.substr(firstIndex, (secIndex - firstIndex) + 1);
-					userInput.replace(firstIndex, (secIndex - firstIndex) + 1, "parenthesisString");
-					parenthesisData.push_back(targetString);
-					//userInput = extractParenthesis(userInput, parenthesisData);
+					userInput = extractParenthesis(userInput, key, parenthesisMap);
 					found = userInput.find('(');
 				}
 			}
-			found = userInput.find('['); //looks for any bracket test commands in userinput and replaces them with just "test"
-			while (found != string::npos) {
-				firstIndex = userInput.find_first_of('[');
-				userInput.replace(firstIndex, 1, "test");
-				secIndex = userInput.find_first_of(']');
-				userInput.erase(secIndex, 1);
-				found = userInput.find('[');
-			}
-
 			//cout << "userInput before parse: " << userInput << endl;
-			rShell* parentExecute = parse(userInput, quotedData, parenthesisData);				
+			rShell* parentExecute = parse(userInput, quotedData, parenthesisMap);				
 			if(!parentExecute->execute()) {
 				cout << "Execution failed" << endl;
 			}
 			print();
  	                getline(cin, userInput);
-
+			key = 0;
+			parenthesisMap.clear();
 		}
 	}
 	exit(0);
@@ -94,16 +90,12 @@ int main() {
 
 void print() { cout << "$ "; }
 
-rShell* parse(string userCommand, vector<string> &quotedData, vector<string> &parenthesisData){
+rShell* parse(string userCommand, vector<string> &quotedData, map<int, string> &parenthesisMap) {
 	vector<string> parser;
 	size_t found1 = userCommand.find(';'); 	  //attempts to find any ; connectors in parsed content
         size_t found2 = userCommand.find("&&");	  //attempts to find any && connectors in parsed content 
         size_t found3 = userCommand.find("||");	  //attempts to find any || connectors in parsed content
 	
-	/*char c_userCommand[userCommand.length() + 1];//have to convert string into array of chars to use strtokpoint = strtok(c_userCommand, "&&");
-        strcpy(c_userCommand, userCommand.c_str());
-        char* point;*/
-
 	string leftParse;
 	string rightParse;
 
@@ -116,17 +108,19 @@ rShell* parse(string userCommand, vector<string> &quotedData, vector<string> &pa
 
 		point = strtok(c_userCommand, " ");
                 while(point != NULL) {
+			string parenthesisString = string(point);
+			string temp = parenthesisString.substr(0, parenthesisString.size() - 1);
                         if(string(point) == "quoteString") {
 				string temp = quotedData.at(0);
                                 temp = temp.substr(1, temp.size() - 2);
                                 parser.push_back(temp);
                                 quotedData.erase(quotedData.begin());
                		}
-			else if (string(point) == "parenthesisString") {
-				string temp = parenthesisData.back();
-				temp = temp.substr(1, temp.size() - 2); //deletes the parenthesis from the sentence
-				parenthesisData.erase(parenthesisData.end());
-				return parse(temp, quotedData, parenthesisData);
+			else if (temp == "parenthesisString") {
+				char charToKey = parenthesisString.back();
+				int key = charToKey - 48;
+				string targetString = parenthesisMap.at(key);
+				return parse(targetString, quotedData, parenthesisMap);
 			}
 			else {
 				parser.push_back(string(point));
@@ -138,82 +132,44 @@ rShell* parse(string userCommand, vector<string> &quotedData, vector<string> &pa
 	}
 
 	else if(found1 != string::npos) {
-		/*point = strtok(c_userCommand, ";");
-                while(point != NULL) {
-                        parser.push_back(string(point));
-                        point = strtok(NULL, ";");
-                }
-		*/
 		leftParse = userCommand.substr(0, found1);
 		rightParse = userCommand.substr(found1 + 1, userCommand.size() - 1);
-                rShell* exec1 = parse(leftParse, quotedData, parenthesisData);
-                rShell* exec2 = parse(rightParse, quotedData, parenthesisData);
+                rShell* exec1 = parse(leftParse, quotedData, parenthesisMap);
+                rShell* exec2 = parse(rightParse, quotedData, parenthesisMap);
                 rShell* semiExec = new ExecuteSEMI(exec1, exec2);
                 return semiExec;
 	}
 
 	else if(found2 != string::npos) {
-		/*point = strtok(c_userCommand, "&&");
-                while(point != NULL) {
-                        parser.push_back(string(point));
-                        point = strtok(NULL, "&&");
-                }*/
 		leftParse = userCommand.substr(0, found2);
                 rightParse = userCommand.substr(found2 + 2, userCommand.size() - 1);
-		rShell* exec1 = parse(leftParse, quotedData, parenthesisData);
-		rShell* exec2 = parse(rightParse, quotedData, parenthesisData);
+		rShell* exec1 = parse(leftParse, quotedData, parenthesisMap);
+		rShell* exec2 = parse(rightParse, quotedData, parenthesisMap);
 		rShell* andExec = new ExecuteAND(exec1, exec2);
 		return andExec;
 	}
 
 	else if(found3 != string::npos) {
-		/*point = strtok(c_userCommand, "||");
-                while(point != NULL) {
-                        parser.push_back(string(point));
-                        point = strtok(NULL, "||");
-                }*/
 		leftParse = userCommand.substr(0, found3);
                 rightParse = userCommand.substr(found3 + 2, userCommand.size() - 1);
-                rShell* exec1 = parse(leftParse, quotedData, parenthesisData);
-                rShell* exec2 = parse(rightParse, quotedData, parenthesisData);
+                rShell* exec1 = parse(leftParse, quotedData, parenthesisMap);
+                rShell* exec2 = parse(rightParse, quotedData, parenthesisMap);
                 rShell* orExec = new ExecuteOR(exec1, exec2);
                 return orExec;
 	}
 }
 
-/*string extractParenthesis(string userInput, vector<string> &parenthesisData) {
+string extractParenthesis(string userInput, int &key, map<int, string> &parenthesisMap) {
 	int firstIndex = userInput.find_first_of('(');
 	int secIndex = userInput.find_first_of(')');
 	if (firstIndex == string::npos && secIndex == string::npos) {
-		parenthesisData.push_back(userInput);
-		return userInput.replace(0, userInput.size(), "parenthesisString");
+		parenthesisMap[key] = userInput;
+		string mapString = "parenthesisString";
+		mapString += (key + 48);
+		++key;
+		return userInput.replace(0, userInput.size(), mapString);
 	}
 	size_t found = userInput.find_first_of('(', firstIndex + 1);
-	string temp;
-	while (found != string::npos) {
-		secIndex = userInput.find_first_of(')', secIndex + 1);
-		found = userInput.find_first_of('(', found + 1);
-	}
-	temp = userInput.substr(firstIndex + 1, (secIndex - firstIndex) - 1);
-	temp = extractParenthesis(temp);
-	if(temp == "parenthesisString") {
-		userInput.replace(firstIndex, (secIndex - firstIndex) + 1, temp);
-		return userInput;
-	}
-	userInput.replace(firstIndex, (secIndex - firstIndex) + 1, extractParenthesis(temp));
-	return userInput;
-}*/
-
-/*string extractParenthesis(string userInput, vector<string> &parenthesisData) {
-	int firstIndex = userInput.find_first_of('(');
-	int secIndex = userInput.find_first_of(')');
-	if (firstIndex == string::npos && secIndex == string::npos) {
-		//push back 
-		parenthesisData.push_back(userInput);
-		return userInput.replace(0, userInput.size(), "parenthesisString");
-	}
-	size_t found = userInput.find_first_of('(', firstIndex + 1);
-	string temp;
 	while (found != string::npos) {
 		if (found > secIndex) {
 			break;
@@ -221,12 +177,13 @@ rShell* parse(string userCommand, vector<string> &quotedData, vector<string> &pa
 		secIndex = userInput.find_first_of(')', secIndex + 1);
 		found = userInput.find_first_of('(', found + 1);
 	}
-	temp = userInput.substr(firstIndex + 1, (secIndex - firstIndex) - 1);
-	temp = extractParenthesis(temp, parenthesisData);
-	if (temp == "parenthesisString") {
+	string temp = userInput.substr(firstIndex + 1, (secIndex - firstIndex) - 1);
+	temp = extractParenthesis(temp, key, parenthesisMap);
+	string parenthesisString = temp.substr(0, temp.size() - 1);
+	if (parenthesisString == "parenthesisString") {
 		userInput.replace(firstIndex, (secIndex - firstIndex) + 1, temp);
 		return userInput;
 	}
-	userInput.replace(firstIndex, (secIndex - firstIndex) + 1, extractParenthesis(temp, parenthesisData));
+	userInput.replace(firstIndex, (secIndex - firstIndex) + 1, extractParenthesis(temp, key, parenthesisMap));
 	return userInput;
-}*/
+}
