5aca163128baf360e4148c9a063c209be11fade2
Ethan White <ewhit004@ucr.edu>
2019-12-10 11:55:47 -0800

    Piping.hpp updated

---
+++ src/piping.hpp
@@ -0,0 +1,28 @@
+#ifndef __PIPING_HPP__
+#define __PIPING_HPP__
+
+#include "rShell.hpp"
+#include <vector>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+using namespace std;
+
+class Piping : public rShell {
+    public:
+    	Piping() : public rShell() {}
+    	Piping(rShell *left, rShell *right) : public rShell() {this->left = left; this->right = right;}
+
+    	bool execute(int in, int out);
+
+    private:
+	rShell *left;
+	rShell *right;
+
+};
+
+#endif
+
